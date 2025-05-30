c37c3e606341737d5ecc412fd49eec7b8bf1a05e
YoussefGri <yyoussef781@gmail.com>
2025-05-30 18:41:36 +0200

    OAuth fix 2

--- src/Controller/OAuthController.php
+++ src/Controller/OAuthController.php
@@ -10,13 +10,25 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class OAuthController extends AbstractController
 {
+
+    private ParameterBagInterface $params;
+
+    public function __construct(ParameterBagInterface $params)
+    {
+        $this->params = $params;
+    }
+
     #[Route('/connect/{provider}', name: 'connect_oauth')]
     public function connectAction(string $provider, ClientRegistry $clientRegistry, Request $request): Response
     {
-        $redirectUrl = $request->query->get('redirect_url', 'http://localhost:8100');
+        // $redirectUrl = $request->query->get('redirect_url', 'http://localhost:8100');
+        $frontendUrl = rtrim($this->params->get('app.frontend_url'), '/');
+        $redirectUrl = $request->getSession()->get('oauth_redirect_url', $frontendUrl);
+
         $session = $request->getSession();
         $session->set('oauth_redirect_url', $redirectUrl);
 
