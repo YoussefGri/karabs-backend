e9c5e0756aff54bc51712d1f40f099360d0bb9ee
YoussefGri <yyoussef781@gmail.com>
2025-05-30 19:25:36 +0200

    OAuth fix 4

--- src/Controller/OAuthController.php
+++ src/Controller/OAuthController.php
@@ -37,49 +37,51 @@ class OAuthController extends AbstractController
             ->redirect(['email', 'profile'], []);
     }
 
-    #[Route('/connect/google/check', name: 'connect_google_check')]
-    public function connectGoogleCheckAction(Request $request, ClientRegistry $clientRegistry, EntityManagerInterface $em, JWTTokenManagerInterface $JWTManager): Response
-    {
-        try {
-            $client = $clientRegistry->getClient('google');
-            $googleUser = $client->fetchUser();
+    // #[Route('/connect/google/check', name: 'connect_google_check')]
+    // public function connectGoogleCheckAction(Request $request, ClientRegistry $clientRegistry, EntityManagerInterface $em, JWTTokenManagerInterface $JWTManager): Response
+    // {
+    //     try {
+    //         $client = $clientRegistry->getClient('google');
+    //         $googleUser = $client->fetchUser();
     
-            $existingUser = $em->getRepository(User::class)->findOneBy([
-                'oauthId' => $googleUser->getId(),
-                'oauthProvider' => 'google'
-            ]);
+    //         $existingUser = $em->getRepository(User::class)->findOneBy([
+    //             'oauthId' => $googleUser->getId(),
+    //             'oauthProvider' => 'google'
+    //         ]);
     
-            if (!$existingUser) {
-                $user = new User();
-                $user->setEmail($googleUser->getEmail());
-                $user->setName($googleUser->getName());
-                $user->setAvatar($googleUser->getAvatar());
-                $user->setOauthId($googleUser->getId());
-                $user->setOauthProvider('google');
-                $user->setRoles(['ROLE_USER']);
+    //         if (!$existingUser) {
+    //             $user = new User();
+    //             $user->setEmail($googleUser->getEmail());
+    //             $user->setName($googleUser->getName());
+    //             $user->setAvatar($googleUser->getAvatar());
+    //             $user->setOauthId($googleUser->getId());
+    //             $user->setOauthProvider('google');
+    //             $user->setRoles(['ROLE_USER']);
     
-                $em->persist($user);
-                $em->flush();
-            } else {
-                $user = $existingUser;
-            }
+    //             $em->persist($user);
+    //             $em->flush();
+    //         } else {
+    //             $user = $existingUser;
+    //         }
     
-            $token = $JWTManager->create($user);
+    //         $token = $JWTManager->create($user);
     
-            // $redirectUrl = $request->getSession()->get('oauth_redirect_url', 'http://localhost:8100');
-            $frontendUrl = rtrim($this->params->get('app.frontend_url'), '/');
-            $redirectUrl = $request->getSession()->get('oauth_redirect_url', $frontendUrl);
-            $request->getSession()->remove('oauth_redirect_url'); // Clear session
+    //         // $redirectUrl = $request->getSession()->get('oauth_redirect_url', 'http://localhost:8100');
+    //         $frontendUrl = rtrim($this->params->get('app.frontend_url'), '/');
+    //         $redirectUrl = $request->getSession()->get('oauth_redirect_url', $frontendUrl);
+    //         $request->getSession()->remove('oauth_redirect_url'); // Clear session
     
-            return $this->redirect($redirectUrl . '/auth/callback?token=' . urlencode($token));
+    //         return $this->redirect($redirectUrl . '/auth/callback?token=' . urlencode($token));
     
-        } catch (\Exception $e) {
-            error_log($e->getMessage());
-            // $redirectUrl = $request->getSession()->get('oauth_redirect_url', 'http://localhost:8100');
-            $frontendUrl = rtrim($this->params->get('app.frontend_url'), '/');
-            $redirectUrl = $request->getSession()->get('oauth_redirect_url', $frontendUrl);
-            $request->getSession()->remove('oauth_redirect_url'); // Clear session
+    //     } catch (\Exception $e) {
+    //         error_log($e->getMessage());
+    //         // $redirectUrl = $request->getSession()->get('oauth_redirect_url', 'http://localhost:8100');
+    //         $frontendUrl = rtrim($this->params->get('app.frontend_url'), '/');
+    //         $redirectUrl = $request->getSession()->get('oauth_redirect_url', $frontendUrl);
+    //         $request->getSession()->remove('oauth_redirect_url'); // Clear session
     
-            return $this->redirect($redirectUrl . '/auth/callback?error=' . urlencode('Erreur d\'authentification: ' . $e->getMessage()));
-        }
-    }}
+    //         return $this->redirect($redirectUrl . '/auth/callback?error=' . urlencode('Erreur d\'authentification: ' . $e->getMessage()));
+    //     }
+    // }
+    
+}
